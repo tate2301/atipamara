7a3a4448c15440fe64079f0ddfe19963096097d4
Kamfeskaya <tatendachris@gmail.com>
2024-05-31 22:40:28 +0200

    fix: change modal sizing

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,27 @@ export default function About() {
   return (
     <div className="max-w-2xl mx-auto p-4 space-y-32 py-32 px-4">
       <div>
-        <h1 className="leading-relaxed mb-6">Today</h1>
+        <h1 className="leading-relaxed mb-6">About me</h1>
         <p className="leading-relaxed mb-4">
           I work as a design engineer with the Innovation Hub at the Chinhoyi
           University of Technology. I help startups, founders, and their teams
           to realize their full potential through thoughtful design and
           engineering.
         </p>
-        <p>
-          In the past I worked with a few startups and companies as a freelance
-          frontend developer and product designer.
+        <p className="leading-relaxed">
+          Over the past 6 years I worked with a few startups and companies
+          across different roles ranging between frontend developer, fullstack
+          developer, product designer and I won&#39;t forget the one experience
+          where I won a{" "}
+          <a
+            href="https://zindi.africa/competitions/umojahack-africa-2021-2-sendy-challenge-intermediate/leaderboard"
+            target="_blank"
+            rel="no-referrer"
+            className="text-[#202020]"
+          >
+            Data Science
+          </a>{" "}
+          challenge.
         </p>
       </div>
 
@@ -57,7 +68,7 @@ export default function About() {
           />
         </div>
       </div>
-      <div>
+      <div className="flex flex-col space-y-4">
         <ButtonWithDisclosure
           backgroundColor="#EF5F00"
           color="#fff"
@@ -66,7 +77,7 @@ export default function About() {
           icon={<ArrowUpRightIcon className="w-6 h-6 stroke-2" />}
         >
           <h1 className="text-3xl font-bold mb-2">
-            Let&lsquo;s talk about your next project
+            Let&lsquo;s talk about working together
           </h1>
           <p className="max-w-2xl text-balance">
             We are always excited to work with new people and help them bring
@@ -74,6 +85,20 @@ export default function About() {
             how we can help you.
           </p>
         </ButtonWithDisclosure>
+
+        <div>
+          <p>
+            See more of my work on{" "}
+            <a href="https://twitter.com/atipamara" className="text-[#202020]">
+              X
+            </a>{" "}
+            or check out my code on{" "}
+            <a href="https://github.com/tate2301" className="text-[#202020]">
+              GitHub
+            </a>
+            . Thanks for visiting.
+          </p>
+        </div>
       </div>
     </div>
   );
--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,63 +1,68 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
 type ModalProps = {
-    children: React.ReactNode;
-    isOpen: boolean;
-    onClose: () => void;
-}
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+};
 
 export default function Modal(props: ModalProps) {
-    useEffect(() => {
-        if (props.isOpen) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "auto";
-        }
+  useEffect(() => {
+    if (props.isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
 
-        return () => {
-            document.body.style.overflowY = "auto";
-        }
-    }, [props.isOpen])
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, [props.isOpen]);
 
-    useEffect(() => {
-        // close on escape key press
-        const handleKeyPress = (e: KeyboardEvent) => {
-            if (e.key === "Escape") {
-                props.onClose();
-            }
-        }
+  useEffect(() => {
+    // close on escape key press
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        props.onClose();
+      }
+    };
 
-        window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("keydown", handleKeyPress);
 
-        return () => {
-            window.removeEventListener("keydown", handleKeyPress);
-        }
-    }, [])
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
-    
-
-    return (
-        <motion.div 
-        initial={{
-            bottom: "-100%",
-            opacity: 0
-        }}
-        animate={{
-            bottom: 0,
-            opacity: 1
-        }} 
-        className="fixed inset-0 bg-zinc-200/80 backdrop-blur-sm z-50 p-16">
-            <motion.div className="flex h-full w-full bg-white rounded-[32px] relative overflow-hidden">
-                <button className="w-[40px] h-[40px] rounded-full bg-black/10 absolute top-4 right-4" onClick={props.onClose}>
-                    <XMarkIcon className="w-6 h-6 m-auto" />
-                </button>
-                <motion.div layout="position" className="bg-white p-8 rounded-lg relative">
-                    {props.children}
-                </motion.div>
-            </motion.div>
+  return (
+    <motion.div
+      initial={{
+        bottom: "-100%",
+        opacity: 0,
+      }}
+      animate={{
+        bottom: 0,
+        opacity: 1,
+      }}
+      className="fixed inset-0 bg-zinc-200/70 backdrop-blur-sm z-50 p-16"
+    >
+      <motion.div className="flex h-full w-full max-w-2xl mx-auto bg-white rounded-[32px] relative overflow-hidden">
+        <button
+          className="w-[40px] h-[40px] rounded-full bg-black/10 absolute top-4 right-4"
+          onClick={props.onClose}
+        >
+          <XMarkIcon className="w-6 h-6 m-auto" />
+        </button>
+        <motion.div
+          layout="position"
+          className="p-8 pt-0 mt-16 rounded-lg relative"
+        >
+          {props.children}
         </motion.div>
-    )
+      </motion.div>
+    </motion.div>
+  );
 }
