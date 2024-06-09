5f57c2011cebd95d670d20e64ca6ce57355d9f40
Custodian <thehalfstackdev@gmail.com>
2024-06-09 15:45:56 +0200

    refactor: move cards to new folder

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 import { Newsreader } from "next/font/google";
+import ProjectCard from "@/components/cards/ProjectCard";
+import WorkExperienceCard from "@/components/cards/WorkExperienceCard";
 
 const serif = Newsreader({
   subsets: ["latin"],
@@ -177,45 +179,3 @@ export default function About() {
     </div>
   );
 }
-
-const ProjectCard = (props: {
-  title: string;
-  description: string;
-  link: string;
-}) => {
-  return (
-    <Link href={props.link}>
-      <p className="text-[#202020] font-medium leading-relaxed capitalize">
-        {props.title}
-      </p>
-      <p>{props.description}</p>
-    </Link>
-  );
-};
-
-const WorkExperienceCard = (props: {
-  title: string;
-  description: string;
-  link: string;
-  timeline: string;
-}) => {
-  // timeline should be a string like "2020 - 2020" or "2020 - Present" or "2020 - 2021"
-  // remove any other characters
-  let timeline = props.timeline.replace(/[^0-9\s-]/g, "");
-  if (props.timeline.includes("Present")) {
-    timeline += " Present";
-  }
-  return (
-    <Link href={props.link} className="flex gap-8">
-      <p className="text-sm leading-relaxed capitalize w-32 flex-shrink-0">
-        {timeline}
-      </p>
-      <div>
-        <p className="text-[#202020] font-medium leading-relaxed capitalize mb-2">
-          {props.title}
-        </p>
-        <p className="text-balance">{props.description}</p>
-      </div>
-    </Link>
-  );
-};
